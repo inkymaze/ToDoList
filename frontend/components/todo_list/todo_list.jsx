e712b777c6bb2186bd6c421fe39185e486d4bf79
Nicholas Cannon <nickwcannon@gmail.com>
2017-07-11 16:58:54 -0700

    the ajax request for creating a new post is functioning with bugs

--- frontend/components/todo_list/todo_list.jsx
+++ frontend/components/todo_list/todo_list.jsx
@@ -1,33 +1,33 @@
-// import React from 'react';
-// // import getAllTodos from '../../reducers/selectors';
-// import TodoListItem from './todo_list_item';
-// import TodoForm from './todo_form';
-//
-//
-// class TodoList extends React.Component { //be aware of {}
-//   // let titlearr = Object.keys(todos).map(title => todos[title]);
-//   componentDidMount() {
-//     this.props.fetchTodos();
-//   }
-// // todos, receiveTodo, removeTodo}
-//   render () {
-//     const {todos, receiveTodo, removeTodo} = this.props;
-//
-//     return (
-//       <ul>
-//         {
-//           todos.map((todo) => (
-//             <li key={todo.id}>
-//               <TodoListItem todo={todo} removeTodo={removeTodo} receiveTodo={receiveTodo}/>
-//             </li>
-//           ))
-//         }
-//         <li>
-//           <TodoForm receiveTodo={receiveTodo} />
-//         </li>
-//       </ul>
-//     );
-//   }
-// }
-//
-// export default TodoList;
+import React from 'react';
+// import getAllTodos from '../../reducers/selectors';
+import TodoListItem from './todo_list_item';
+import TodoForm from './todo_form';
+
+
+class TodoList extends React.Component { //be aware of {}
+  // let titlearr = Object.keys(todos).map(title => todos[title]);
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
+// todos, receiveTodo, removeTodo}
+  render () {
+    const {todos, receiveTodo, removeTodo} = this.props;
+
+    return (
+      <ul>
+        {
+          todos.map((todo) => (
+            <li key={todo.id}>
+              <TodoListItem todo={todo} removeTodo={removeTodo} receiveTodo={receiveTodo}/>
+            </li>
+          ))
+        }
+        <li>
+          <TodoForm receiveTodo={receiveTodo} />
+        </li>
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
