da2d0f957530f394eae17ad5506923cb3e5f82b7
Nicholas Cannon <nickwcannon@gmail.com>
2017-07-11 16:59:19 -0700

    The second ajax request is now functioning without bugs

--- frontend/components/todo_list/todo_form.jsx
+++ frontend/components/todo_list/todo_form.jsx
@@ -1,46 +1,46 @@
-// import React from 'react';
-// import {uniqueId} from '../../util/unique_id';
-//
-//
-// class TodoForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       title: "",
-//       body: "",
-//       id: '',
-//       done: false
-//     };
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-//
-//   handleSubmit (e) {
-//     e.preventDefault();
-//     const todo = Object.assign({}, this.state);
-//     // console.log(this);
-//     todo.id = uniqueId();
-//     this.props.receiveTodo(todo);
-//   }
-//
-//   update (property) {
-//     return (event)=>{
-//       this.setState({[property]: event.target.value});
-//     };
-//   }
-//
-//   render() {
-//     return (
-//       <form onSubmit={this.handleSubmit}>
-//         <label>Title</label>
-//         <input type="text" onChange={this.update("title")} />
-//         <label>Body</label>
-//         <input type="text" onChange={this.update("body")} />
-//         <input type="submit" value="Submit Form" />
-//       </form>
-//
-//     );
-//   }
-// }
-//
-//
-// export default TodoForm;
+import React from 'react';
+import {uniqueId} from '../../util/unique_id';
+
+
+class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      body: "",
+      id: '',
+      done: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    const todo = Object.assign({}, this.state);
+    // console.log(this);
+    todo.id = uniqueId();
+    this.props.receiveTodo(todo);
+  }
+
+  update (property) {
+    return (event)=>{
+      this.setState({[property]: event.target.value});
+    };
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Title</label>
+        <input type="text" onChange={this.update("title")} />
+        <label>Body</label>
+        <input type="text" onChange={this.update("body")} />
+        <input type="submit" value="Submit Form" />
+      </form>
+
+    );
+  }
+}
+
+
+export default TodoForm;
